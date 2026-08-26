04a56217b1713501b939ab707c96d53a4a0a77e1
Faraj Arshad <faraj.arshad01@gmail.com>
2026-08-27 00:21:36 +0500

    Syntax error fixed

--- src/hopsworksclients.py
+++ src/hopsworksclients.py
@@ -52,7 +52,7 @@ def get_or_create_feature_group():
             ),
             primary_key=["datetime"],
             event_time="datetime",
-            online_enabled=True
+            online_enabled=True,
             time_travel_format="HUDI"
         )
     )

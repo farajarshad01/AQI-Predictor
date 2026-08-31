448ccee059c1d5f486311b8e7ee818d243e457af
Faraj Arshad <faraj.arshad01@gmail.com>
2026-08-31 16:10:47 +0500

    Modify sys.path to include project root
    Add project root to sys.path for module imports

--- app/streamlit_dashboard.py
+++ app/streamlit_dashboard.py
@@ -1,3 +1,15 @@
+import os
+import sys
+
+PROJECT_ROOT = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
